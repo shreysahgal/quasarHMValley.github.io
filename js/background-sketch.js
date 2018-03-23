2e325d775d502b7d94b1e684526c2e66162570a1
shreysahgal <shreysahgal@gmail.com>
2018-03-23 23:28:27 +0200

    change font and background
    Changed font form robot-monospace to just roboto, changed color of navbar li's (still subject to further chage), and changed the background to off-white (same as navbar)

--- js/background-sketch.js
+++ js/background-sketch.js
@@ -1,4 +1,4 @@
-var alphaMax = 100;
+var al = 100;
 var ovalY = 375;
 
 var a1 = 64;
@@ -10,7 +10,7 @@ function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.style('z-index', '-1');
     canvas.position(0, 0);
-    
+
     ellipseMode(CENTER);
 }
 
@@ -21,34 +21,40 @@ function windowResized() {
 
 
 function draw() {
-    clear();
-    
+    background(239, 239, 239);
+
     noStroke();
     // orange oval
-    fill(255, 156, 0, 100);
+    fill(255, 156, 0, al);
     push();
     translate(windowWidth / 2, ovalY)
     rotate(radians(a1));
     ellipse(0, 0, 650, 200);
     pop();
-    
+
     // purple oval
-    fill(207, 209, 255, 170);
+    fill(207, 209, 255, al+10);
     push();
     translate(windowWidth / 2, ovalY)
     rotate(radians(a2));
     ellipse(0, 0, 650, 200);
     pop();
-    
+
     // pink oval
-    fill(255, 185, 187, 100);
+    fill(255, 185, 187, al);
     push();
     translate(windowWidth / 2, ovalY)
     rotate(radians(a3));
     ellipse(0, 0, 650, 200);
     pop();
-    
+
     a1 += 1;
     a2 -= 2;
     a3 += 0.5;
+}
+
+function mouseWheel() {
+    if (al - event.delta <= 100) {
+        al -= event.delta / 3;
+    }
 }
