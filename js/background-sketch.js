4b0e4f5385eab9b00563f3eb80e3a42a136ea905
shreysahgal <shreysahgal@gmail.com>
2018-04-03 17:00:37 -0400

    fixed some of the fade stuff
    made the fade of the ovals better annd made the fade of the text based on the same scroll variable as the oval in javascript

--- js/background-sketch.js
+++ js/background-sketch.js
@@ -5,25 +5,79 @@ var a1 = 64;
 var a2 = 10;
 var a3 = -30;
 
+var a1change = 1;
+var a2change = -2;
+var a3change = 0.5;
+
+var scroll;
+var scrollLimit;
+
+var titlePos;
+
+var blablabla, x, y;
+
+function getPosition(element) {
+    var xPosition = 0;
+    var yPosition = 0;
+
+    while(element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+
+    return { x: xPosition, y: yPosition };
+}
+
+function updateAlpha() {
+    al = map(scroll, 0, scrollLimit, 100, -300);
+    if (al < 0) {
+        al = 0
+    }
+}
+
+// check if page has loaded (to make sure "body" isn't null)
+function pageLoaded() {
+    return document.readyState === "complete";
+}
 
 function setup() {
-    canvas = createCanvas(windowWidth, windowHeight);
-    canvas.style('z-index', '-1');
+
+    if (pageLoaded) {
+        scrollLimit = Math.max(document.body.scrollHeight,
+            document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+        
+        blablabla = document.querySelector("#blablabla");
+    }
+
+    canvas = createCanvas(windowWidth, scrollLimit);
+    canvas.style('z-index', '-1'); // put canvas in background
     canvas.position(0, 0);
 
     ellipseMode(CENTER);
+
+    scroll = window.scrollY;
 }
 
+// when window is resized
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-    background(0);
-}
+    scrollLimit = Math.max(document.body.scrollHeight,
+        document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
 
+    resizeCanvas(windowWidth, windowHeight);
+}
 
 function draw() {
+    if (pageLoaded()) {
+        blablabla = document.getElementById("blablabla");
+//        console.log(rect.top, rect.left)
+    }
+    titlePos = document.getElementsByClassName("main").scrollTop;
+    // console.log(titlePos);
     background(239, 239, 239);
 
     noStroke();
+
     // orange oval
     fill(255, 156, 0, al);
     push();
@@ -33,7 +87,7 @@ function draw() {
     pop();
 
     // purple oval
-    fill(207, 209, 255, al+10);
+    fill(207, 209, 255, al + 10);
     push();
     translate(windowWidth / 2, ovalY)
     rotate(radians(a2));
@@ -48,13 +102,20 @@ function draw() {
     ellipse(0, 0, 650, 200);
     pop();
 
-    a1 += 1;
-    a2 -= 2;
-    a3 += 0.5;
-}
+    fill(0);
+    textSize(30);
 
-function mouseWheel() {
-    if (al - event.delta <= 100) {
-        al -= event.delta / 3;
-    }
+    // things that change (should be last)
+
+    a1 += a1change;
+    a2 -= a2change;
+    a3 += a3change;
+
+    scroll = window.scrollY; // update scroll loc.
+
+    updateAlpha();
+    
+//    console.log(map(scroll, 0, scrollLimit, 1, 0));
+    document.getElementById("fadescrollMain").style.opacity = map(scroll, 0, scrollLimit, 1, -5).toString();
+    document.getElementById("fadescrollSub").style.opacity = map(scroll, 0, scrollLimit, 1, -5).toString();
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-$(window).scroll(function(){
-    $(".fadescroll").css("opacity", 1 - $(window).scrollTop() / 250);
-  });
+//$(window).scroll(function(){
+//    $(".fadescroll").css("opacity", 1 - $(window).scrollTop() / 250);
+//  });
+//
+//var parent = document.getElementById('container1');
+//var child = document.getElementById('container2');
+//child.style.paddingRight = child.offsetWidth - child.clientWidth + "px";
